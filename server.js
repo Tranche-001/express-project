ee4135cfa257bd6a719d289a1053f1da370bc9fa
Tranche-001 <tranche100@gmail.com>
2025-09-22 20:09:32 -0300

    set inital ejs to render views properly

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 import express from "express"
 import * as dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import path from 'path';
 import {newRouter} from "./routes/newRouter.js"
 import { indexRouter } from "./routes/indexRouter.js";
 dotenv.config();
 const PORT = process.env.PORT;
 
 const app = express();
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//Usage of ejs
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+//Enabling css
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
 
 app.use("/new", newRouter);
 app.use("/", indexRouter);
