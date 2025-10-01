4ca49b3f8c341286007990534a6c21787a7e41c5
Tranche-001 <tranche100@gmail.com>
2025-10-01 19:56:40 -0300

    create getAllMessages query

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,7 @@
+import { queries } from "../db/queries.js";
 import { messages } from "../models/db.js";
 
-export const renderIndexPage = (req, res) => {
+export const renderIndexPage = async (req, res) => {
+  const execute = await queries.getAllMessagesDB();
   res.render("index",  { title: "Mini Messageboard", messages: messages });
 }
--- db/queries.js
+++ db/queries.js
@@ -2,11 +2,14 @@ import { pool } from "./pool";
 
 export const queries = (() => {
 
-
+  const getAllMessagesDB = async () => {
+    const users = await pool.query("SELECT * FROM messages");
+    console.log(users);
+  }
   const updateMessageDB = async (messageUser, messageText) => {
     const newUser = {username: messageUser, text: messageText, added: new Date()};
     await pool.query("INSERT INTO messages VALUES ($1)", [newUser]);
   }
 
-  return {updateMessageDB}
+  return {getAllMessagesDB, updateMessageDB}
 })();
