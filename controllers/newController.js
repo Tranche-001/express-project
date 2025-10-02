4d8382fb197949e631e874574b3a1572bac81d93
Tranche-001 <tranche100@gmail.com>
2025-10-02 14:24:54 -0300

    create validator and sanitizer for user inputs

--- controllers/newController.js
+++ controllers/newController.js
@@ -5,8 +5,21 @@ export const renderFormPage = (req, res) => {
   res.render("form")
 }
 
+const alphaErr = "must only contain letters.";
+const lengthErr = "must be between 1 and 10 characters.";
+const textErr = "can't have more than 200 characters"
+
+
+const validateUserInput = [
+  body("messageUser").isAlpha().withMessage(`username ${alphaErr}`)
+    .isLength({ min: 1, max: 10 }).withMessage(`username${lengthErr}`)
+    .escape(),
+  body("messageText").optional({ checkFalsy: true }).isLength({ max: 200 }).withMessage(`Text ${textErr}`)
+  .escape(),
+]
 export const updateMessageData = async (req, res) => {
-  const {messageUser, messageText} = req.body;
+  const { messageUser, messageText } = req.body;
+
   await queries.updateMessageDB(messageUser, messageText);
   res.redirect("/");
 }
